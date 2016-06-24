d76dcb74df9e7e1d600b6bc0ef7eb4cf1ba0bebc
Dusty Mabe <dusty@dustymabe.com>
2016-06-24 15:18:14 -0400

    Clean up output and make git pull work.

--- prod.sh
+++ prod.sh
@@ -9,30 +9,30 @@ BRANCH=production
 git checkout $BRANCH &> /dev/null
 
 function build() {
-  echo "### Updating git branch '$BRANCH'...\n"
-  #git pull
-  # TODO insert git update commands
+  echo -e "### Updating git branch '$BRANCH'...\n"
+  git pull origin $BRANCH
   SHA1="$(git log --pretty=format:'%h' -n 1)"
-  echo "### Fetched revision $SHA1\n"
-  echo "### Running Maven build...\n"
-  # TODO insert maven command, redirect all output to /dev/null
-  mvn package #&> /dev/null
-  echo "### Built ticket-monster.war using maven\n"
+  echo -e "### Fetched revision $SHA1\n"
 
-  echo "### Build/Deploy to production using ansible-container...\n"
+
+  echo -e "### Running Maven build...\n"
+  mvn package #&> /dev/null
+  echo -e "### Built ticket-monster.war using maven\n"
+
+  echo -e "### Build and deploy to production using ansible-container...\n"
   cp target/ticket-monster.war misc/Dockerfiles/ticketmonster-ha/ticket-monster.war
   pushd misc/
   docker run -it --rm -v $(pwd):/work/ -e DETACH=1 -e DOCKER_HOST dustymabe/ansible-container --debug run
   popd
 
-  echo "### Brought up TicketMonster using ansible-container\n"
+  echo -e "### Brought up TicketMonster using ansible-container\n"
   docker ps
 }
 
-echo "### Polling for updates to git...\n"
+echo -e "### Polling for updates to git...\n"
 while true; do
   git fetch &> build_log.txt
-  echo "$BRANCH"
+  echo -n '.'
   if [ -s build_log.txt ]; then
     build
     exit
