ff8c8e9631521bc95380fc3bb1d90605f49b3b5b
Dusty Mabe <dusty@dustymabe.com>
2016-06-24 14:51:18 -0400

    Clean up some of the flow/wording

--- prod.sh
+++ prod.sh
@@ -18,20 +18,18 @@ function build() {
   # TODO insert maven command, redirect all output to /dev/null
   mvn package #&> /dev/null
   echo "### Built ticket-monster.war using maven\n"
-  echo "### Running Linux container builds...\n"
+
+  echo "### Build/Deploy to production using ansible-container...\n"
   cp target/ticket-monster.war misc/Dockerfiles/ticketmonster-ha/ticket-monster.war
   pushd misc/
   docker run -it --rm -v $(pwd):/work/ -e DETACH=1 -e DOCKER_HOST dustymabe/ansible-container --debug run
   popd
 
-  # TODO insert container builds, redirect all output to /dev/null
-  echo "Built Linux container for TicketMonster + WildFly\n"
-  echo "Starting ansible-container to bring up containers in production...\n"
-  # TODO insert ansible container start, redirect all output to /dev/null
-  echo "Brought up TicketMonster using ansible-container"
+  echo "### Brought up TicketMonster using ansible-container\n"
   docker ps
 }
 
+echo "### Polling for updates to git...\n"
 while true; do
   git fetch &> build_log.txt
   echo "$BRANCH"
