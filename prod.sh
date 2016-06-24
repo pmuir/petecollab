0cca7e24b259a638917c7d4a0bf7c5f27b10ffad
Dusty Mabe <dusty@dustymabe.com>
2016-06-24 16:00:01 -0400

    Add controls for where output goes.

--- prod.sh
+++ prod.sh
@@ -4,28 +4,32 @@ export OPENSHIFT_HOST="${OPENSHIFT_HOST:=10.1.2.3:8443}"
 export DOCKER_HOST="${DOCKER_HOST:=tcp://10.1.2.2:2376}"
 WORKSPACE=${WORKSPACE:=/sharedfolder/github.com/eivantsov/ticketmonster/}
 
-cd $WORKSPACE &> /dev/null
+OUTPUT='/dev/null'
+OUTPUT='/dev/stdout'
+
+cd $WORKSPACE &> $OUTPUT
 
 BRANCH=production
-git checkout $BRANCH &> /dev/null
+git checkout $BRANCH &> $OUTPUT
+
 
 function build() {
   echo -e "### Updating git branch '$BRANCH'...\n"
-  git pull origin $BRANCH
+  git pull origin $BRANCH &> $OUTPUT 
   SHA1="$(git log --pretty=format:'%h' -n 1)"
   echo -e "### Fetched revision $SHA1\n"
 
   echo -e "### Running Maven build...\n"
-  mvn package #&> /dev/null
-  cp target/ticket-monster.war misc/Dockerfiles/ticketmonster-ha/ticket-monster.war
+  mvn package &> $OUTPUT
+  cp target/ticket-monster.war misc/Dockerfiles/ticketmonster-ha/ticket-monster.war &> $OUTPUT
   # Silently deploy/build in openshift
-  
+  openshift &> $OUTPUT
   echo -e "### Built ticket-monster.war using maven\n"
 
   echo -e "### Build and deploy to production using ansible-container...\n"
-  pushd misc/
-  docker run -it --rm -v $(pwd):/work/ -e DETACH=1 -e DOCKER_HOST dustymabe/ansible-container --debug run
-  popd
+  pushd misc/ &> $OUTPUT
+  docker run -it --rm -v $(pwd):/work/ -e DETACH=1 -e DOCKER_HOST dustymabe/ansible-container --debug run &> $OUTPUT
+  popd &> $OUTPUT
 
   echo -e "### Brought up TicketMonster using ansible-container\n"
   docker ps
